9fbacd4c1e3fd8e9e260f3de85e0dd283f9c2060
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-20 08:49:18 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,11 +11,11 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, k;
-	int temp;
+	int temp, swap_count = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		for (k = 0; k < size - i; k++)
+		for (k = 0; k <= size - i - 1; k++)
 		{
 			if (array[k] > array[k + 1])
 			{
@@ -24,7 +24,10 @@ void bubble_sort(int *array, size_t size)
 				array[k + 1] = temp;
 
 				print_array(array, size);
+				swap_count++;
 			}
 		}
+		if (swap_count == 0)
+			break;
 	}
 }
