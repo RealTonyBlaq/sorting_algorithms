e10274d002f743a6801219cf82a45e599069a790
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-20 08:53:47 +0100

    sorts only when sizeof array > 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,21 +13,24 @@ void bubble_sort(int *array, size_t size)
 	size_t i, k;
 	int temp, swap_count = 0;
 
-	for (i = 0; i < size; i++)
+	if (size > 1)
 	{
-		for (k = 0; k <= size - i - 1; k++)
+		for (i = 0; i < size; i++)
 		{
-			if (array[k] > array[k + 1])
+			for (k = 0; k <= size - i - 1; k++)
 			{
-				temp = array[k];
-				array[k] = array[k + 1];
-				array[k + 1] = temp;
+				if (array[k] > array[k + 1])
+				{
+					temp = array[k];
+					array[k] = array[k + 1];
+					array[k + 1] = temp;
 
-				print_array(array, size);
-				swap_count++;
+					print_array(array, size);
+					swap_count++;
+				}
 			}
+			if (swap_count == 0)
+				break;
 		}
-		if (swap_count == 0)
-			break;
 	}
 }
