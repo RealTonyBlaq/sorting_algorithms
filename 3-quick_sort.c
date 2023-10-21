72d046e6645c31108680ee79744434ab896c28d6
stephen_adah <mailstephenreal@gmail.com>
2023-10-21 07:55:46 -0700

    in progress

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap - function that swap the indexes of the array
+ * @x: first index to the swap
+ * @y: the second index to be swap
+ * Return: Nothing
+ */
+void swap(int *x, int *y)
+{
+	int z;
+
+	z = *x;
+	*x = *y;
+	*y = z;
+}
+
+/**
  * partition - function that choose the pivot
  * @arr: The array to be sorted
  * @low: the lowest index of array
@@ -9,10 +24,26 @@
  */
 int partition(int arr[], int low, int high)
 {
-	int pivot;
+	int pivot, j, se_index; /*se_index - small element index*/
 
 	pivot = arr(high);
+	se_index = low - 1;
+
+	for (j = low; j <= high; j++)
+	{
+		/*check if element if smaller than the pivot*/
+		if (arr[j] < pivot)
+		{
+			/*increment index of smaller element*/
+			se_index++;
+			swap(&arr[se_index], &arr[j]);
+			print_array(arr, high + 1);
+		}
+	}
+	swap(&arr[se_index + 1], &arr[high]);
+	return (se_index + 1);
 }
+
 /**
  * quickSort - function that sort the array
  * @arr: The array to be sorted
