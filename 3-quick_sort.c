db455ea89cfcd1dd9f9dfa7c2866ab56ed5bd839
stephen_adah <mailstephenreal@gmail.com>
2023-10-21 07:21:05 -0700

    in progress

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,37 @@
 #include "sort.h"
+
 /**
+ * quickSort - function that sort the array
+ * @arr: The array to be sorted
+ * @low: lowest index notation of the array
+ * @high: Highest index notation of the array
+ * Return: Nothing
+ */
+void quickSort(int arr[], int low, int high)
+{
+	int partition_index;
+
+	if (low >= high || low < 0)
+		return;
+	partition_index = partition(arr, low, high);
+}
+/**
+ * quick_sort -  function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the arry
+ * Return: Nothing
  */
 void quick_sort(int *array, size_t size)
 {
+	int low, high;
 
+	low = 0;
+	high = size - 1;
+
+	if (size >= 0 || size < 0)
+	{
+		return;
+	}
+	quickSort(arr, low, high);
 }
