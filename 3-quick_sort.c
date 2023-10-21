780d9b100f902d39f627848b56d4eee7572163df
stephen_adah <mailstephenreal@gmail.com>
2023-10-21 07:34:41 -0700

    in progress

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,19 @@
 #include "sort.h"
 
 /**
+ * partition - function that choose the pivot
+ * @arr: The array to be sorted
+ * @low: the lowest index of array
+ * @high: The highest index of array
+ * Return: an integer value
+ */
+int partition(int arr[], int low, int high)
+{
+	int pivot;
+
+	pivot = arr(high);
+}
+/**
  * quickSort - function that sort the array
  * @arr: The array to be sorted
  * @low: lowest index notation of the array
@@ -9,12 +22,22 @@
  */
 void quickSort(int arr[], int low, int high)
 {
-	int partition_index;
+	int p_index;
 
 	if (low >= high || low < 0)
+	{
 		return;
-	partition_index = partition(arr, low, high);
+	}
+	if (low < high)
+	{
+		p_index = partition(arr, low, high);
+		/*sort the left subarray*/
+		quickSort(arr, low, p_index - 1);
+		/*sort the right subarray*/
+		quickSort(arr, p_index + 1, high);
+	}
 }
+
 /**
  * quick_sort -  function that sorts an array of integers
  * in ascending order using the Quick sort algorithm
