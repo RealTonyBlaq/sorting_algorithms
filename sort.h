f5d0d36f87aa6e711c3ef5ee3f6a3fc215b3677e
stephen_adah <mailstephenreal@gmail.com>
2023-10-23 11:54:43 -0700

    modified

--- sort.h
+++ sort.h
@@ -29,6 +29,7 @@ int partition(int arr[], size_t size, int low, int high);
 void swap(int *x, int *y);
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+int get_maximum(int *arr, int size);
 void cocktail_sort_list(listint_t **list);
 int check_sorted(listint_t **list);
 
